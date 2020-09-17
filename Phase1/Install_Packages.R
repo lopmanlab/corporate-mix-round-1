ea58136cc70f66f9939e8e27b7ce721e7f943f92
jbatross <jonnybatross@gmail.com>
2020-09-17 10:03:33 -0400

    Updated to include lapply

--- Phase1/Install_Packages.R
+++ Phase1/Install_Packages.R
@@ -1,4 +1,4 @@
-### Package Installation
+## Package installation & loading -------
 list.of.packages <- c(
   "reshape2",
   "ggplot2",
@@ -13,4 +13,6 @@ list.of.packages <- c(
 )
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
+invisible(lapply(list.of.packages,library,character.only=T))
 rm(list.of.packages,new.packages) #Removes lists for cleanliness
+
